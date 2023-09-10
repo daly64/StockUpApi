a7a8672502edddea9933448a257696ad38007c05
Daly <dalyna64@gmail.com>
2023-09-10 12:38:34 +0100

    Initial commit

---
+++ src/main/java/com/daly/stockupapi/StockUpApiApplication.java
@@ -0,0 +1,13 @@
+package com.daly.stockupapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockUpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockUpApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daly/stockupapi/StockUpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daly.stockupapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockUpApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
